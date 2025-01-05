57d7d9e3f1c2c05a4be1e0f4d50cbe34a7643bb0
Jose Ferreira <jferreira.5655@unimar.edu.ve>
2025-01-05 14:08:53 -0400

    agregados endpoint con filtros de los otros modelos

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('itineraries', views.get_itineraries), #ruta para obtener todos los itinerarios
+    path('itinerary/<int:itinerary_id>', views.get_itinerary), #ruta para obtener un itinerario
     path('itineraries/agent/<int:agent_id>/', views.get_itineraries_by_agent), #ruta para obtener los itinerarios de un agente
+    path('itineraries/leg/<int:leg_id>/', views.get_itineraries_with_leg), #ruta para obtener los itinerarios que contienen una leg
     path('legs', views.get_legs), #ruta para obtener todas las legs
+    path('leg/<int:leg_id>', views.get_leg), #ruta para obtener una leg
     path('airlines', views.get_airlines), #ruta para obtener todas las aerolineas
+    path('airline/<int:airline_id>', views.get_airline), #ruta para obtener una aerolinea
     path('agents', views.get_agents), #ruta para obtener todos los agentes
+    path('agent/<int:agent_id>', views.get_agent), #ruta para obtener un agente
     path('airports', views.get_airports), #ruta para obtener todos los aeropuertos
+    path('airport/<int:airport_id>', views.get_airports), #ruta para obtener un aeropuerto
 ]
--- api/views.py
+++ api/views.py
@@ -12,6 +12,12 @@ def get_itineraries(request): #Obtener todos los itinerarios
     return Response(serializer.data)
 
 @api_view(["GET"])
+def get_itinerary(request, itinerary_id): #Obtener un itinerario
+    itinerary = Itinerary.objects.filter(id=itinerary_id)
+    serializer = ItinerarySerializer(itinerary, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
 def get_itineraries_by_agent(request, agent_id): #Obtener itinerarios de un agente
     if(agent_id):
         itineraries = Itinerary.objects.filter(agent_id=agent_id)
@@ -25,9 +31,28 @@ def get_itineraries_by_agent(request, agent_id): #Obtener itinerarios de un agen
         return Response("No se ha proporcionado un agente")
 
 @api_view(["GET"])
+def get_itineraries_with_leg(request, leg_id): #Obtener itinerarios que contengan una leg especifica
+    if(leg_id):
+        itineraries = Itinerary.objects.filter(legs__id=leg_id)
+        
+        if not (itineraries.exists()):
+            return Response("No se encontraron itinerarios que contengan esta leg")
+        
+        serializer = ItinerarySerializer(itineraries, many=True)
+        return Response(serializer.data)
+    else:
+        return Response("No se ha proporcionado una leg")
+
+@api_view(["GET"])
 def get_legs(request): #Obtener todas las legs
     legs = Leg.objects.all()
     serializer = LegSerializer(legs, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def get_leg(request, leg_id): #Obtener una leg
+    leg = Leg.objects.filter(id=leg_id)
+    serializer = LegSerializer(leg, many=True)
     return Response(serializer.data)
 
 @api_view(["GET"])
@@ -37,13 +62,31 @@ def get_airlines(request): #Obtener todas las aerolineas
     return Response(serializer.data)
 
 @api_view(["GET"])
+def get_airline(request, airline_id): #Obtener una aerolinea
+    airline = Airline.objects.filter(id=airline_id)
+    serializer = AirlineSerializer(airline, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
 def get_agents(request): #Obtener todos los agentes
     agents = Agent.objects.all()
     serializer = AgentSerializer(agents, many=True)
     return Response(serializer.data)
 
 @api_view(["GET"])
+def get_agent(request, agent_id): #Obtener un agente
+    agent = Agent.objects.filter(id=agent_id)
+    serializer = AgentSerializer(agent, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
 def get_airports(request):
     airports = Airport.objects.all() #Obtener todos los aeropuertos
     serializer = AirportSerializer(airports, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def get_airport(request, airport_id):
+    airport = Airport.objects.filter(id=airport_id) #Obtener un aeropuerto
+    serializer = AirportSerializer(airport, many=True)
     return Response(serializer.data)
